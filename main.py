5637302e15b64c9848073751719d6db712801d12
Shane McDonough <shanemcdman@gmail.com>
2019-12-27 04:39:47 -0800

    Create class coord

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import os
 from colorama import init, Fore
-
 init()
+
+class coord:
+    def __init__(self, xcoord, ycoord):
+        self.x = xcoord
+        self.y = ycoord
 
 def gotoxy(x, y):
     print("\033[%d;%dH" % (y, x))
 
 
+head = coord(9,5)
 
 os.system("cls")
 print("test")
-gotoxy(9,9)
+gotoxy(head.x, head.y)
 print("test")
