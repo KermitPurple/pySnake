b1d9805c60bdaf5dc8fe325f1955aebd138f9d44
Shane McDonough <shanemcdman@gmail.com>
2020-01-15 14:59:47 -0800

    Made a bot that traces the walls

--- bot.py
+++ bot.py
@@ -1,4 +1,12 @@
 def bot_move(fruit, direction, height, width, tail, head):
     #create logic
-    return 'd'
-
+    if head.y >= height and direction != 'a':
+        return 'a'
+    elif head.y <= 1 and direction != 'd':
+        return 'd'
+    elif head.x >= width and direction != 's':
+        return 's'
+    elif head.x <= 1 and direction != 'w':
+        return 'w'
+    else:
+        return direction
