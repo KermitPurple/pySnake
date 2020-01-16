885fa617d7a488c635a503064a8d73fc517a7137
Shane McDonough <shanemcdman@gmail.com>
2020-01-16 14:02:55 -0800

    Create panic mode and fix bug dying before border

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ def bot_move(fruit, direction, height, width, tail, head):
         if valid_move(fruit, direction, height, width, tail, head, 'w'):
             return 'w'
     else:
-        if valid_move(fruit, direction, height, width, tail, head, 'w'):
+        if valid_move(fruit, direction, height, width, tail, head, direction):
             return direction
         else:
             panic_mode(fruit, direction, height, width, tail, head)
@@ -30,7 +30,7 @@ def valid_move(fruit, direction, height, width, tail, head, potential_direction)
         new_pos = coord(head.x, head.y + 1)
     elif potential_direction == 'd':
         new_pos = coord(head.x + 1, head.y)
-    if new_pos.y > height or new_pos.y <= 1 or new_pos.x > width or new_pos.y <= 1:
+    if new_pos.y > height or new_pos.y < 1 or new_pos.x > width or new_pos.x < 1:
         return False
     for point in tail:
         if new_pos.x == point.x and new_pos.y == point.y:
@@ -38,4 +38,9 @@ def valid_move(fruit, direction, height, width, tail, head, potential_direction)
     return True
 
 def panic_mode(fruit, direction, height, width, tail, head):
-    pass
+    for potential_direction in ['w','a','s','d']:
+        if valid_move(fruit, direction, height, width, tail, head, potential_direction):
+            return potential_direction
+        else:
+            return direction
+
