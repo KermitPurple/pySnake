88a78bd90643bb1b505cd1aef850883e425b34db
Shane McDonough <shanemcdman@gmail.com>
2020-01-16 13:57:47 -0800

    Change logic circuit and create panic prototype

--- bot.py
+++ bot.py
@@ -2,26 +2,40 @@ from coord import coord
 
 def bot_move(fruit, direction, height, width, tail, head):
     #create logic
-    if head.x < fruit.x and direction != 'd' and direction != 'a':
+    if head.x < fruit.x:
         if valid_move(fruit, direction, height, width, tail, head, 'd'):
             return 'd'
-    elif head.x > fruit.x and direction != 'd' and direction != 'a':
+    if head.x > fruit.x:
         if valid_move(fruit, direction, height, width, tail, head, 'a'):
             return 'a'
-    elif head.y < fruit.y and direction != 'w' and direction != 's':
+    if head.y < fruit.y:
         if valid_move(fruit, direction, height, width, tail, head, 's'):
             return 's'
-    elif head.y > fruit.y and direction != 'w' and direction != 's':
+    if head.y > fruit.y:
         if valid_move(fruit, direction, height, width, tail, head, 'w'):
             return 'w'
     else:
-        return direction
+        if valid_move(fruit, direction, height, width, tail, head, 'w'):
+            return direction
+        else:
+            panic_mode(fruit, direction, height, width, tail, head)
+
 
 def valid_move(fruit, direction, height, width, tail, head, potential_direction):
-   # if potential_direction == 'w':
-   # elif potential_direction == 'a':
-   # elif potential_direction == 's':
-   # elif potential_direction == 'd':
-   # for point in tail:
-
+    if potential_direction == 'w':
+        new_pos = coord(head.x, head.y - 1)
+    elif potential_direction == 'a':
+        new_pos = coord(head.x - 1, head.y)
+    elif potential_direction == 's':
+        new_pos = coord(head.x, head.y + 1)
+    elif potential_direction == 'd':
+        new_pos = coord(head.x + 1, head.y)
+    if new_pos.y > height or new_pos.y <= 1 or new_pos.x > width or new_pos.y <= 1:
+        return False
+    for point in tail:
+        if new_pos.x == point.x and new_pos.y == point.y:
+            return False
     return True
+
+def panic_mode(fruit, direction, height, width, tail, head):
+    pass
