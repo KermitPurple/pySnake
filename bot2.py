531200891b11d18e2413205e8e199fb227c17414
Shane McDonough <shanemcdman@gmail.com>
2020-01-17 20:55:00 -0800

    actually add completeed bot2.py

---
+++ bot2.py
@@ -0,0 +1,21 @@
+from coord import coord
+
+def bot_move(fruit, direction, height, width, tail, head):
+    if head.x == 1:
+        if head.y == 1:
+            return 'd'
+        return 'w'
+    elif head.x == width:
+        if head.y == height:
+            return 'a'
+        return 's'
+    elif head.y == 2:
+        if head.x % 2 == 1:
+            return 'a'
+        return 's'
+    elif head.y == height:
+        if head.x % 2 == 1:
+            return 'w'
+        return 'a'
+    else:
+        return direction
