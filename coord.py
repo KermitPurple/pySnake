121c256c6f2401de62d0afe49b0ed7a289a3a309
Shane McDonough <shanemcdman@gmail.com>
2020-01-19 22:39:45 -0800

    include __eq__ in coord

--- coord.py
+++ coord.py
@@ -2,3 +2,9 @@ class coord:
     def __init__(self, xcoord = 1, ycoord = 1):
         self.x = xcoord
         self.y = ycoord
+    
+    def __eq__(self, other):
+        if self.x == other.x and self.y == other.y:
+            return True
+        else:
+            return False
